e36b3d3f317280324d75beb2326c8f3c0c4e3cfa
Francis Mangala <fdmangala@yahoo.com>
2025-03-23 09:29:03 -0400

    Update Repo

---
+++ booklover_test_run.py
@@ -0,0 +1,8 @@
+from booklover.booklover import BookLover
+
+new_book = BookLover("Rebecca Yarros","yarros@book.com","Fiction")
+
+new_book.add_book("Fourth Wing",4)
+
+print(f"{new_book.num_books}")
+
